ab12130fbdff4f7f3747651e28f7bdb23440a78a
Ayush Chothe <ayushmchothe@gmail.com>
2023-07-16 02:18:52 +0530

    domain fix

--- standalone/scripts/src/push_hls.sh
+++ standalone/scripts/src/push_hls.sh
@@ -3,6 +3,6 @@ until [ -f /tmp/raw/$1.m3u8 ]; do
     sleep 5
 done
 ffmpeg -re -i /tmp/raw/$1.m3u8 \
-    -c:a aac -b:a 32k -c:v libx264 -b:v 128K -s 852x480 -ar 44100 -ac 2 -tune zerolatency -preset veryfast -f flv rtmp://server/task/$1_low \
-    -c:a aac -b:a 64k -c:v libx264 -b:v 256k -s 1280x720 -ar 44100 -ac 2 -tune zerolatency -preset veryfast -f flv rtmp://server/task/$1_mid \
-    -c:a aac -b:a 128k -c:v libx264 -b:v 512K -s 1920x1080 -ar 44100 -ac 2 -tune zerolatency -preset veryfast -f flv rtmp://server/task/$1_hi
+    -c:a aac -b:a 32k -c:v libx264 -b:v 128K -s 852x480 -ar 44100 -ac 2 -tune zerolatency -preset veryfast -f flv rtmp://localhost/task/$1_low \
+    -c:a aac -b:a 64k -c:v libx264 -b:v 256k -s 1280x720 -ar 44100 -ac 2 -tune zerolatency -preset veryfast -f flv rtmp://localhost/task/$1_mid \
+    -c:a aac -b:a 128k -c:v libx264 -b:v 512K -s 1920x1080 -ar 44100 -ac 2 -tune zerolatency -preset veryfast -f flv rtmp://localhost/task/$1_hi
